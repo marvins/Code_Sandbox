8bef0dcf7ca1868ed14f0076cc8bcc82b5cf15e5
Marvin Smith <ms6401@ms6401-VirtualBox.(none)>
2012-03-22 10:53:58 -0700

    adding base unit test library

---
+++ unit_test/src/Logger.h
@@ -0,0 +1,41 @@
+#ifndef __SRC_TEST_LOGGER_H__
+#define __SRC_TEST_LOGGER_H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Color.h"
+
+using namespace std;
+
+class Status{
+
+   public:
+      Status();
+      Status( const string& f, const int& r, const string& n = "");
+
+      string func_name;
+      string note;
+      int result;
+
+};
+ostream& operator << (ostream& ostr, const Status& stat );
+
+class Logger{
+
+   public:
+
+      void append( const Status& stat );
+      
+      void print_log( );
+
+      vector<Status> log;
+};
+
+void print_module_header( const string& header_name );
+void print_module_footer( const string& header_name );
+
+void print_test_results( const string& func_name, const int& result, const string& note="");
+
+#endif
