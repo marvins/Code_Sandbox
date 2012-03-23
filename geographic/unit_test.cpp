656182625f70ab32eebd725c2d73b836e612b2fb
Marvin Smith <marvin_smith1@me.com>
2012-03-22 23:13:27 -0700

    minor additions

--- geographic/unit_test.cpp
+++ geographic/unit_test.cpp
@@ -116,56 +116,18 @@ int  TEST_NITF_initialization( string& note){
 
 int  TEST_NITF_get_image( string& note ){
 
-    vector<GeoImage> imgArr(45);
-    imgArr[ 0].set_filename("data/U_1001A.NTF");
-    imgArr[ 1].set_filename("data/U_1005A.NTF");
-    imgArr[ 2].set_filename("data/U_1025A.NTF");
-    imgArr[ 3].set_filename("data/U_1033A.NTF");
-    imgArr[ 4].set_filename("data/U_1034A.NTF");
-    imgArr[ 5].set_filename("data/U_1036A.NTF");
-    imgArr[ 6].set_filename("data/U_1050A.NTF");
-    imgArr[ 7].set_filename("data/U_1051E.NTF");
-    imgArr[ 8].set_filename("data/U_1052A.NTF");
-    imgArr[ 9].set_filename("data/U_1056A.NTF");
-    imgArr[10].set_filename("data/U_1060A.NTF");
-    imgArr[11].set_filename("data/U_1064A.NTF");
-    imgArr[12].set_filename("data/U_2001A.NTF");
-    imgArr[13].set_filename("data/U_2001D.NTF");
-    imgArr[14].set_filename("data/U_2001E.NTF");
-    imgArr[15].set_filename("data/U_2005A.NTF");
-    imgArr[16].set_filename("data/U_2010A.NTF");
-    imgArr[17].set_filename("data/U_2015A.NTF");
-    imgArr[18].set_filename("data/U_2020A.NTF");
-    imgArr[19].set_filename("data/U_3001A.NTF");
-    imgArr[20].set_filename("data/U_3002A.NTF");
-    imgArr[21].set_filename("data/U_3010A.NTF");
-    imgArr[22].set_filename("data/U_4003B.NTF");
-    imgArr[23].set_filename("data/U_4017A.NTF");
-    imgArr[24].set_filename("data/test01.ntf");
-    imgArr[25].set_filename("data/test02.ntf");
-    imgArr[26].set_filename("data/test03.ntf");
-    imgArr[27].set_filename("data/test04.ntf");
-    imgArr[28].set_filename("data/test05.ntf");
-    imgArr[29].set_filename("data/test06.ntf");
-    imgArr[30].set_filename("data/test07.ntf");
-    imgArr[31].set_filename("data/test08.ntf");
-    imgArr[32].set_filename("data/test10.ntf");
-    imgArr[33].set_filename("data/test11.ntf");
-    imgArr[34].set_filename("data/test12.ntf");
-    imgArr[35].set_filename("data/test13.ntf");
-    imgArr[36].set_filename("data/test20.ntf");
-    imgArr[37].set_filename("data/test21.ntf");
-    imgArr[38].set_filename("data/test22.ntf");
-    imgArr[39].set_filename("data/test23.ntf");
-    imgArr[40].set_filename("data/test24.ntf");
-    imgArr[41].set_filename("data/test25.ntf");
-    imgArr[42].set_filename("data/test26.ntf");
-    imgArr[43].set_filename("data/test27.ntf");
-    imgArr[44].set_filename("data/test28.ntf");
+    vector<GeoImage> imgArr;
 
-    for(size_t i=0; i<imgArr.size(); i++)
-        imgArr[i].set_init(true);
-
+    ifstream fin;
+    fin.open("data/run_file.txt");
+    string fname;
+    fin >> fname;
+    while( !fin.eof()){
+      imgArr.push_back( GeoImage(fname, true));
+      fin >> fname;
+    }
+    fin.close();
+    
     Mat currentImg;
     namedWindow("IMAGE");
 
