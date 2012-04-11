b20ec728890052b1477501c46f44607cc6edfeaa
Marvin Smith <ms6401@cengpgpu02.sncorp.intranet.com>
2012-04-11 12:30:19 -0600

    added boost support

--- geographic/unit_test.cpp
+++ geographic/unit_test.cpp
@@ -65,34 +65,34 @@ int  TEST_NITF_Constructor( string& note){
     GeoImage img3("data/U_1025A.NTF", true );
     //GeoImage img4("data/U_1033A.NTF", true );
     //GeoImage img5("data/U_1034A.NTF", true );
-    //GeoImage img6;
-
-/*    
+    GeoImage img6;
+    
     //test init function 
     note = "Initialization string improperly initialized on img";
     if( img1.get_init() == true  ){ note += "1"; return false; }
     if( img2.get_init() == true  ){ note += "2"; return false; }
     if( img3.get_init() == false ){ note += "3"; return false; }
-    if( img4.get_init() == false ){ note += "4"; return false; }
-    if( img5.get_init() == false ){ note += "5"; return false; }
+    //if( img4.get_init() == false ){ note += "4"; return false; }
+    //if( img5.get_init() == false ){ note += "5"; return false; }
     if( img6.get_init() == true  ){ note += "6"; return false; }
 
-    //test set init
+    //test set init function
     note = "Set init fails on set_init call";
     img6.set_init(true);
     if( img6.get_init() == true || img6.get_filename() != ""){ return false; }
+   
+    //give it valid filename and try again
     img6.set_filename("data/U_1036A.NTF");
     img6.set_init(true);
     if( img6.get_init() != true || img6.get_filename() != "data/U_1036A.NTF" )
         return false;
-*/
+    
     note = "Test successful";
     return true;
 }
 
 int  TEST_NITF_initialization( string& note){
 
-/*
     GeoImage img1("data/U_1001A.NTF", true );
     GeoImage img2("data/U_1005A.NTF", true );
     GeoImage img3("data/U_1025A.NTF", true );
@@ -121,7 +121,6 @@ int  TEST_NITF_initialization( string& note){
     if( string(img6.getDriver()->GetDescription()) != "NITF" )
     { note += img6.getDriver()->GetDescription();  return false; }
 
-*/
     note = "Test successful";
     return true;
 }
