1c01f5b5d222a91c7b7e24f6ba912f179e82ffdd
Marvin Smith <marvin_smith1@me.com>
2012-04-28 17:12:21 -0700

    adding some useful math libraries

---
+++ math/calculus/Runge_Kutta.h
@@ -0,0 +1,10 @@
+#ifndef __CALCULUS_RUNGE_KUTTA_H__
+#define __CALCULUS_RUNGE_KUTTA_H__
+
+class Runge_Kutta{
+
+
+
+};
+
+#endif
---
+++ math/core/Vector.cpp
@@ -0,0 +1,14 @@
+#include "Vector.h"
+
+/**
+ * Cross product between two vectors
+ * @param a first vector
+ * @param b second vector
+ * @return cross product vector
+ */
+vec cross( const vec& a, const vec& b ){
+    
+    return vec( a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]);
+}
+
+
