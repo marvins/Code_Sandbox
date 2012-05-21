79092fcb22a41b385884a47c699a0495c958ead3
Marvin Smith <ms6401@cengpgpu02.sncorp.intranet.com>
2012-05-21 11:00:42 -0600

    ImageStitching has working point comparison

--- opencv/ImageStitching.cpp
+++ opencv/ImageStitching.cpp
@@ -1,13 +1,15 @@
-#include <cv.h>
-#include <cvaux.h>
-#include <highgui.h>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 
 #include <fstream>
 #include <iostream>
 #include <utility>
 #include <vector>
 
-#include <opencv2/features2d/features2d.hpp>
 
 using namespace cv;
 using namespace std;
@@ -27,6 +29,7 @@ vector<Mat> load_images( string const& filename );
 vector<string>  read_image_list( string const& filename );
 void show_image( Mat const& img );
 void FindMatches( Mat const& img1, Mat const& img2, vector<pntMatch>& matches );
+void convertMatch2Mat(const vector <pntMatch> &matches, Mat &src, Mat &dst);
 
 
 int main( int argc, char* argv[] ){
@@ -34,9 +37,9 @@ int main( int argc, char* argv[] ){
     namedWindow("WINDOW");
 
     //create list of images
-    vector<Mat> images = load_images( argv[1] );
+    vector<Mat> images      = load_images( argv[1] );
     vector<Mat> gray_images = convertBGR2Gray( images );
-
+    
     //build image metrics
     vector< ImageModel >  imageModel( gray_images.size() );
     for( size_t i=0; i<gray_images.size(); i++ ){
@@ -47,9 +50,21 @@ int main( int argc, char* argv[] ){
     //find correspondencies between images
     vector<pntMatch> matches;
 
-    for( size_t i=0; i< imageModel.size(); i++)
+    for( size_t i=1; i< imageModel.size(); i++){
+        
+        //find matching keypoints between images
         FindMatches( imageModel[i-1].gimg, imageModel[i].gimg, matches );
+        
+        //convert match vector to Mats
+        Mat pnts1, pnts2;
+        convertMatch2Mat( matches, pnts1, pnts2 );
+        
+        //compute homography matrix
+        vector<uchar> mask;
+        Mat homography = findHomography( pnts2, pnts1, mask, CV_RANSAC, 2.0 );
 
+
+    }
 
 
     return 0;
@@ -72,7 +87,6 @@ vector<Mat> convertBGR2Gray( vector<Mat>const& images ){
 vector<Mat> load_images( string const& filename ){
 
     vector<string> filenames = read_image_list( filename );
-
     vector<Mat> images;
 
     for( size_t i=0; i<filenames.size(); i++){
@@ -110,21 +124,52 @@ void show_image( Mat const& img ){
 
 }
 
-void FindMatches( Mat const& img1, Mat const& img2, vector<pntMatch>& matches ){
+void FindMatches( Mat const& img1, Mat const& img2, vector<pntMatch>& match_list ){
 
+    //compute keypoints
     SurfFeatureDetector detector(400);
     vector<KeyPoint> keypoints1, keypoints2;
     detector.detect( img1, keypoints1 );
     detector.detect( img2, keypoints2 );
+    
+    //compute descriptors
+    SurfDescriptorExtractor extractor;
+    Mat descriptors1, descriptors2;
+    extractor.compute( img1, keypoints1, descriptors1 );
+    extractor.compute( img2, keypoints2, descriptors2 );
+    
+    //find matches
+    vector<DMatch> matches;
+	FlannBasedMatcher matcher;
+    matcher.match(descriptors1, descriptors2, matches);
 
+    match_list.clear();
+    for( int i = 0; i < (int)matches.size(); i++) {
+        int i1 = matches[i].queryIdx;
+        int i2 = matches[i].trainIdx;
 
+        const KeyPoint &kp1 = keypoints1[i1];
+        const KeyPoint &kp2 = keypoints2[i2];
+
+        // This is ad-hoc, adjust to suit your need
+        if(matches[i].distance < 0.05) {
+            match_list.push_back( pntMatch(kp1.pt,kp2.pt));
+        }
+    }
 }
 
 
+/******************************************************************************/
+void convertMatch2Mat(const vector <pntMatch> &matches, Mat &src, Mat &dst)
+{
+    src = Mat::zeros(matches.size(), 2, CV_32F);
+    dst = Mat::zeros(matches.size(), 2, CV_32F);
 
+    for(unsigned int i=0; i < matches.size(); i++) {
+        src.at<float>(i,0) = matches[i].first.x;
+        src.at<float>(i,1) = matches[i].first.y;
 
-
-
-
-
-
+        dst.at<float>(i,0) = matches[i].second.x;
+        dst.at<float>(i,1) = matches[i].second.y;
+    }
+}
