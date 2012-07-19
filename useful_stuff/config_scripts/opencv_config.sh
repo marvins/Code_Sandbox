b2985584805bbf02497af8e9d4df17f04807a829
Marvin Smith <ms6401@ubuntu.(none)>
2012-07-19 15:37:00 -0700

    renaming configure script

---
+++ useful_stuff/config_scripts/opencv_config.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+echo running opencv config
+
+OPENCV_REPO_LOCATION="http://code.opencv.org/svn/opencv"
+
+echo OpenCV Management Script
+
+
+#############################################
+#                   USAGE                   #
+#############################################
+usage(){
+
+cat <<EOF
+
+Usage:
+    `basename $0` download
+EOF
+}
+
+
+case "$1" in
+
+    download )
+        svn checkout ${OPENCV_REPO_LOCATION}
+        ;;
+
+    update )
+        svn up
+        ;;
+    
+    build )
+        mkdir -p release
+        cd release
+        cmake .. 
+
+    * )
+        usage
+
+esac
+
+
+
+
+
+
