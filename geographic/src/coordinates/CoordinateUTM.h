a3d75d224814e7da3bfb32410f9bcf2dab421205
Marvin Smith <marvin_smith1@me.com>
2012-08-02 23:19:25 -0700

    adding utm core

---
+++ geographic/src/coordinates/CoordinateUTM.h
@@ -0,0 +1,13 @@
+#ifndef __SRC_COORDINATES_COORDINATEUTM_H__
+#define __SRC_COORDINATES_COORDINATEUTM_H__
+
+
+class CoordinateUTM : public CoordinateBase {
+    
+    public:
+        
+        CoordinateUTM();
+
+};
+
+#endif
