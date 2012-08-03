583d8d10951f5d201a803d5f9a4ab62435cf4a7b
Marvin Smith <marvin_smith1@me.com>
2012-08-02 23:20:44 -0700

    adding mgrs core

---
+++ geographic/src/coordinates/CoordinateMGRS.h
@@ -0,0 +1,13 @@
+#ifndef __SRC_COORDINATES_COORDINATEMGRS_H__
+#define __SRC_COORDINATES_COORDINATEMGRS_H__
+
+
+class CoordinateMGRS : public CoordinateBase {
+    
+    public:
+        
+        CoordinateMGRS();
+
+};
+
+#endif
