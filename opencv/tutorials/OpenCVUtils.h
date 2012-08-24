a162eeb5352921b03e6f189b3bac2fba937b50ab
Marvin Smith <marvin.smith@sncorp.com>
2012-08-23 22:14:03 -0700

    adding more tutorials

---
+++ opencv/tutorials/OpenCVUtils.h
@@ -0,0 +1,12 @@
+#ifndef __SRC_OPENCV_ENUMS_H__
+#define __SRC_OPENCV_ENUMS_H__
+
+#include <cv.h>
+#include <cvaux.h>
+#include <highgui.h>
+
+using namespace cv;
+
+int cvDepthChannel2Type( const int Depth, const int Channels );
+
+#endif
