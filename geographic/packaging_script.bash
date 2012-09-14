70805177b5610a3d949fcfd81dffd6539b2a10cb
Marvin Smith <ms6401@cengpgpu02.sncorp.intranet.com>
2012-09-14 12:49:38 -0600

    finally starting to work

--- geographic/packaging_script.bash
+++ geographic/packaging_script.bash
@@ -3,10 +3,14 @@
 # This is the location of the Medusa repository
 MEDUSA_PATH=/home/ms6401/projects/Medusa
 
-GEOIMAGE_HOME=geographic
+GEOIMAGE_SRC=src
 GEOIMAGE_DEST=GeoImage
 
+# Make sure destination directory exists
+if [ ! -d "${MEDUSA_PATH}/${GEOIMAGE_DEST}" ]; then 
+    mkdir ${MEDUSA_PATH}/${GEOIMAGE_DEST}
+fi
 
 # Copy over geoimage code
-cp -r ${GEOIMAGE} ${MEDUSA_PATH}/${GEOIMAGE_DEST}
+cp -r ${GEOIMAGE_SRC} ${MEDUSA_PATH}/${GEOIMAGE_DEST}/${GEOIMAGE_SRC}
 
