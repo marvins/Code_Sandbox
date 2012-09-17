59e282ef729e0ac018999290bafe0845e4507245
Marvin Smith <marvin.smith@sncorp.com>
2012-09-16 23:10:32 -0700

    adding header demo to geo image

---
+++ geographic/geo_image/docs/examples/GS2_Header_Demo.cpp
@@ -0,0 +1,22 @@
+#include <GeoImage.h>
+
+#include <opencv2/core/core.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main( int argc, char** argv ){
+
+
+    //create the geoimage
+    GEO::GeoImage  gimg( argv[1], true);
+
+    //pull the position of the sensor 
+    Mat sensor_position = GEO::GS2::getSensorPosition( gimg );
+
+
+    return 0;
+}
