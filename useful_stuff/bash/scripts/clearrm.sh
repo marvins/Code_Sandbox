d6e7c498f25108058bdb2aef40ffb1e52bf29698
Marvin Smith <marvin.smith@sncorp.com>
2012-12-31 18:50:35 -0700

    fixed script to add it to the crontab

--- useful_stuff/bash/scripts/clearrm.sh
+++ useful_stuff/bash/scripts/clearrm.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+# Parse all command-line options
+flags=""
+while getopts "f" opt
+    do
+        case $opt in
+        f)
+            rm -rf ~/.deleted_files/*
+            cat "deleted on $(date)" >> /home/ms6401/Desktop/archived_cleared.txt
+            exit 0
+            ;;
+        *)
+        ;;
+       esac
+    done
+
+
 echo -n "Are you sure you wish to clear the archive? (y/n) [n]: "
 read desired
+
 
 if [ ${desired:="n"} == "y" ]; then
     rm -rf ~/.deleted_files/*
