7711411683aba0384b9e908692148c2bc5dd35dd
Marvin Smith <marvin.smith@sncorp.com>
2013-01-06 17:49:36 -0800

    putting in the structure

--- useful_stuff/bash/scripts/gs2-manager.sh
+++ useful_stuff/bash/scripts/gs2-manager.sh
@@ -11,7 +11,20 @@ gs2_manager( ){
     if [ "$1" == "GUI" ]; then
         GUI=1
     fi
+    
+    # Give a set of options if in GUI mode
+    OPTION=0
+    while [ $GUI -eq 1 ]; do
 
+        # Print the header
+
+
+        # Give the user input
+
+    done
+    if [ ! $GUI -eq 1 ]; then
+
+    fi
 
 
 }
