03684e8dec383e269eb0a35ba1faf593cd512d06
Marvin Smith <marvin.smith@sncorp.com>
2013-03-29 01:31:47 -0700

    fixed stupid dash requirements. ignoring now

--- c++/boost/configure_linux.sh
+++ c++/boost/configure_linux.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 PREFIX=/opt/local
 
 read -p "Do you wish to run the bootstrap script? (y/n - default): "  ANS
-
-if [ "$ANS" == 'y' -o "$ANS" == "Y" ]; then
+if [ $ANS = 'y' ]  || [ $ANS = 'Y' ]; then 
     ./bootstrap.sh --prefix=$PREFIX
 fi
 
 read -p 'Do you wish to build boost? (y/n - default): ' ANS
-
-if [ "$ANS" == "y" -o "$ANS" == "Y" ]; then
-    sudo ./b2 --install
+if [ $ANS = 'y' ]  || [ $ANS = 'Y' ]; then 
+    echo "WORLD" #sudo ./b2 --install
 fi
 
