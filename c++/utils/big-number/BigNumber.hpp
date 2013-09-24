d023acd184d3dd7bb343a32fc2529710c3fe1059
Marvin Smith <marvin.smith@sncorp.com>
2013-09-24 14:28:03 -0700

    adding base code for big number library

---
+++ c++/utils/big-number/BigNumber.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file   BigNumber.hpp
+ * @author Marvin Smith
+ * @date   9/24/2013
+ */
+#ifndef __BIG_NUMBER_HPP__
+#define __BIG_NUMBER_HPP__
+
+
+#endif
+
---
+++ c++/utils/big-number/big_number_test.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file   big_number_test.cpp
+ * @author Marvin Smith
+ * @date   9/24/2013
+ */
+#include <BigNumber.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char* argv[] ){
+
+    // create a few big numbers
+    BigInteger value01;
+    
+
+    return 0;
+}
+
