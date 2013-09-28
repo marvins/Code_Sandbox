13505d2154b510936671d0cc3ca877617b167730
Marvin Smith <marvin.smith@sncorp.com>
2013-09-27 19:33:08 -0700

    adding euler60 base code

---
+++ misc/Project_Euler/euler60/euler60.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file    euler60.cpp
+ * @author  Marvin Smith
+ * @date    9/27/2013
+ */
+#include <iostream>
+
+#include <Primesieve.hpp>
+
+using namespace std;
+
+
+int main( int argc, char* argv[] ){
+
+    // create prime sieve
+    Primes  primesieve(999999);
+
+        
+
+    return 0;
+}
+
