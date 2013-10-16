454970c3a586b2d4ee2872636c78e8acc198d5df
Marvin Smith <marvin_smith1@me.com>
2013-10-15 19:59:11 -0700

    code should work

--- misc/Project_Euler/euler61/euler61.cpp
+++ misc/Project_Euler/euler61/euler61.cpp
@@ -3,6 +3,7 @@
  * @author Marvin Smith
  * @date   10/15/2013
 */
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <utility>
@@ -12,10 +13,23 @@
 
 using namespace std;
 
-int Pjn( int const& j, int const& n ){
+bool is_cyclic( vector<string> const& slist ){
+
+    for( size_t i=0; i<slist.size()-1; i++ ){
+        if( slist[i][slist[i].size()-2] != slist[i+1][0] ) return false;
+        if( slist[i][slist[i].size()-1] != slist[i+1][1] ) return false;
+    }
     
+    if( slist[slist.size()-1][slist[slist.size()-1].size()-2] != slist[0][0] ) return false;
+    if( slist[slist.size()-1][slist[slist.size()-1].size()-1] != slist[0][1] ) return false;
+
+    return true;
+}
+
+int Pjn( int const& j, int const& n ){
+
     switch( j ){
-        
+
         case 3:
             return (n*(n+1)/2);
 
@@ -45,21 +59,21 @@ int Pjn( int const& j, int const& n ){
 
 
 int main( int argc, char* argv[] ){
-   
+
     try{
-       
+
         // find the indeces where numbers become 4 digits
         vector<pair<int,int> > indeces(9);
         bool break1 = false;
         bool break2 = false;
         for( size_t i=3; i<=8; i++ ){
-        
+
             // cound indeces
             size_t x=0;
             break1 = false;
             break2 = false;
             while( break2 != true ){
-            
+
                 // convert value to string
                 string val=num2str(Pjn(i,x));
 
@@ -79,47 +93,67 @@ int main( int argc, char* argv[] ){
         // start building the sets
         vector<vector<string> > values(9);
         for( size_t i=3; i<=8; i++ ){
-            
+
             // push all numbers to the list
             for( size_t j=indeces[i].first; j<indeces[i].second; j++ )
-                values[i].push_back( num2str(Pjn( j, i)) );
+                values[i].push_back( num2str(Pjn( i, j)) );
         }
 
         // iterate through all items
-        while( true ){
-            
+        bool breakLoop = false;
+        while( !breakLoop ){
+
+            // create temp list
+            vector<string> tempList(6);
+
             // create list of items
-            for( size_t a=0; a<values[3].size(); a++ )
-            for( size_t b=0; b<values[4].size(); b++ )
-            for( size_t c=0; c<values[5].size(); c++ )
-            for( size_t d=0; d<values[6].size(); d++ )
-            for( size_t e=0; e<values[7].size(); e++ )
             for( size_t f=0; f<values[8].size(); f++ ){
-                
-                // add everything to the list
-                vector<string> tempList;
-                tempList.push_back( values[3][a] );
-                tempList.push_back( values[4][b] );
-                tempList.push_back( values[5][c] );
-                tempList.push_back( values[6][d] );
-                tempList.push_back( values[7][e] );
-                tempList.push_back( values[8][f] );
+            for( size_t e=0; e<values[7].size(); e++ ){
+            for( size_t d=0; d<values[6].size(); d++ ){
+            for( size_t c=0; c<values[5].size(); c++ ){
+            for( size_t b=0; b<values[4].size(); b++ ){
+            for( size_t a=0; a<values[3].size(); a++ ){
+                    
             
+                // add everything to the list
+                tempList[0] = values[3][a];
+                tempList[1] = values[4][b];
+                tempList[2] = values[5][c];
+                tempList[3] = values[6][d];
+                tempList[4] = values[7][e];
+                tempList[5] = values[8][f];
+
                 // sort permutations
                 std::sort( tempList.begin(), tempList.end() );
 
                 // test permutations
                 do{
+
                     // make sure none of the values are the same
                     for( size_t v=0; v<tempList.size()-1; v++ ){
-                        if( tempList[v] == tempList[v-1] ){
+                        if( tempList[v] == tempList[v+1] ){
                             break;
                         }
                     }
 
-                    // check front and back 
+                    // check front and back
+                    if( is_cyclic( tempList ) == true ){
+                        cout << "Output" << endl;
+                        for( size_t v=0; v<tempList.size()-1; v++ )
+                            cout << tempList[v] << endl;
+                        breakLoop = true;
+                    }
 
-                } while( std::next_permutations( tempList.begin(), tempList.end() ))
+                } while( std::next_permutation( tempList.begin(), tempList.end() ));
+
+            }
+            cout << "idx: " << b << "/" << values[4].size() << ", " 
+                            << c << "/" << values[5].size() << ", " 
+                            << d << "/" << values[6].size() << ", " 
+                            << e << "/" << values[7].size() << ", " 
+                            << f << "/" << values[8].size() << endl;
+            }}}}
+
             }
         }
 
@@ -127,7 +161,7 @@ int main( int argc, char* argv[] ){
     } catch ( string e ){
         cout << e << endl;
     }
-    
+
 
     return 0;
 }
