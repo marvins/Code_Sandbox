c251b1b1c11c75ddbe96e3c75610798a78e15679
Marvin Smith <marvin_smith1@me.com>
2014-02-24 09:25:46 -0800

    Adding toLower and toUpper functions to string utilities.

--- c++/utilities/string_functions/string_utilities.cpp
+++ c++/utilities/string_functions/string_utilities.cpp
@@ -1,10 +1,11 @@
 #include "string_utilities.hpp"
 
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/trim.hpp>
 
 using namespace std;
 
-/**
+/*
  * Trim a string of leading and trailing spaces. 
 */
 std::string string_trim( const string& str ){
@@ -14,10 +15,35 @@ std::string string_trim( const string& str ){
     return tstr;
 }
 
+/*
+ * Split the string
+ */
 std::vector<std::string> string_split( const std::string& str, const std::string& pattern ){
 
     vector<string> output;
     boost::split( output, str, boost::is_any_of(pattern));
     return output;
 }
+
+
+/*
+ * Convert string to lower case
+ */
+std::string string_toLower( const std::string& in ){
+    
+    std::string value = in;
+    std::transform( in.begin(), in.end(), value.begin(), ::tolower);
+    return value;
+}
+
+/*
+ * Convert string to upper case
+ */
+std::string string_toUpper( const std::string& in ){
+
+    std::string value = in;
+    std::transform( in.begin(), in.end(), value.begin(), ::toupper);
+    return value;
+}
+
 
--- c++/utilities/string_functions/string_utilities.hpp
+++ c++/utilities/string_functions/string_utilities.hpp
@@ -4,6 +4,13 @@
 #include <sstream>
 #include <string>
 
+/**
+ * Convert a string into a number
+ *
+ * @param[in] value String value to convert
+ * 
+ * @return Numerical equivalent
+ */
 template <typename TP>
 TP str2num( std::string const& value ){
     
@@ -14,6 +21,13 @@ TP str2num( std::string const& value ){
     return result;
 }
 
+/**
+ * Convert a number into a string
+ *
+ * @param[in] value Numerical value to convert
+ *
+ * @return String representation
+ */
 template <typename TP>
 std::string num2str( TP const& value ){
 
@@ -25,13 +39,43 @@ std::string num2str( TP const& value ){
 
 /**
  * Trim whitespace from the ends of a string
+ *
+ * @param[in] str  String to trim
+ *
+ * @return Trimmed string
 */
 std::string string_trim( const std::string& str );
 
 /**
  * Split a string using a pattern
+ *
+ * @param[in] str  String to split
+ * @param[in] pattern Regex pattern to split by
+ *
+ * @return list of strings which were split from the input.
 */
 std::vector<std::string> string_split( const std::string& str, const std::string& pattern );
+
+
+/**
+ * Convert string to lower-case
+ *
+ * @param[in] in input string
+ *
+ * @return lower-case string
+ */
+std::string string_toLower( std::string const& in );
+
+/**
+ * Convert string to upper-case
+ *
+ * @param[in] in input string
+ *
+ * @return upper-case string
+ */
+std::string string_toUpper( std::string const& in );
+
+
 
 
 #endif
