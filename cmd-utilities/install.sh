055e1fbd15b4334cf0d7bc791494efdbc6a2f4e3
Marvin Smith <marvin.smith@sncorp.com>
2014-04-08 12:01:15 -0700

    Adding some command-line utilities I am desparately needing.

---
+++ cmd-utilities/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#    Author:  Marvin Smith
+#    Date:    4/8/2014
+#
+#    Purpose:  Install command-line utilities to the desired location
+#
+
+PREFIX=/opt/local/bin
+
+cp binary2decimal "$PREFIX/"
+cp decimal2binary "$PREFIX/"
+
+
