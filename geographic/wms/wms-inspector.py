40fc04852063070386df65bedac8943b2e59a97e
Marvin Smith <marvin.smith@sncorp.com>
2016-02-09 10:51:13 -0800

    Adding test code for WMS opersions using lib.

---
+++ geographic/wms/wms-inspector.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+#  Libraries
+from owslib.wms import WebMapService
+
+
+
+
+
+def Main():
+
+    #  Parse the Command-Line 
+    return 0
+
+
+if __name__ == '__main__':
+    Main()
+
