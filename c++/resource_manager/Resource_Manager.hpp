d7f9e4714f8b5a62c22a5f7712831417946a8d09
Marvin Smith <marvin.smith@sncorp.com>
2020-05-19 17:03:53 -0700

    Working on resource manager.

--- c++/resource_manager/Resource_Manager.hpp
+++ c++/resource_manager/Resource_Manager.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
+template <typename ResourceType>
+class ResourceBuilderBase
+{
+
+};
+
+template <typename ResourceType>
 class Resource_Manager
 {
-public:
+    public:
+
+        Resource_Manager( );
+
+        virtual ~Resource_Manager() = default;
 
 };
--- c++/resource_manager/demo.cpp
+++ c++/resource_manager/demo.cpp
@@ -1,7 +1,9 @@
 
 #include <iostream>
 
+
 int main( int argc, char* argv[] )
 {
+    auto default_builder = []{ return };
     return 0;
 }
